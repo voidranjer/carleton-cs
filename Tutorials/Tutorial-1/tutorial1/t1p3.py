462b35e70f74feedfb5f1e7f602d389980743167
meekranjer <logicism81@gmail.com>
2022-09-29 21:23:46 -0400

    Unzipped Tutorial 1

---
+++ Tutorials/Tutorial-1/tutorial1/t1p3.py
@@ -0,0 +1,17 @@
+from random import uniform
+
+def main():
+    # uniform() is inclusive -> [1, 100], not [1, 100)
+    random_int = round(uniform(1, 100))
+    guess = int(input("Guess a number between 1 and 100: "))
+    diff = abs(guess - random_int)
+    
+    # checks if win/lose, and prints out difference
+    if random_int == guess:
+        print(f"You won! The number was {random_int}, just like what you guessed!")
+    else:
+        print(f"Your guess was {guess}, but the number was {random_int}!")
+        print(f"You were off by {diff}.")
+
+if __name__ == "__main__":
+    main()
