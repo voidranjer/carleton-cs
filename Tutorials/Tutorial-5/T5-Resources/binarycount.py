6928df64d05a66061eee1697fd19d27958a677d1
meekranjer <logicism81@gmail.com>
2022-10-16 13:43:08 -0400

    Submission 1

--- Tutorials/Tutorial-5/T5-Resources/binarycount.py
+++ Tutorials/Tutorial-5/T5-Resources/binarycount.py
@@ -49,11 +49,15 @@ x = []
 y = []
 xLength = 2500
 yLength = 50000
+xDuplicates = 1
 
 for i in range(xLength):  # list to search in
-    x.append(random.randint(-10, 10))
+    for _ in range(xDuplicates):  # to test duplicate values in X
+        num = random.randint(-10, 10)
+        x.append(num)
 
 for j in range(yLength):  # search list, aka numbers that will be searched for
+    # to test case number 4 (larger proportions of Y not present in X, increase the range of randint in Y relative to X)
     y.append(random.randint(-10000, 10000))
 
 x.sort()  # sort the list
@@ -68,6 +72,29 @@ print("Average time for binary search count: " + str(binaryTimeAvg))
 print("Average time for python count(): " + str(pythonTimeAvg))
 
 # Analysis:
-# 1. The binary search count is faster than the built-in count function
-# 2. Doubling x causes a smaller increase in time taken for the binary search count function [O(log n)], but doubles the time taken for the linear built-in search function [O(n)]
-# 3.
+# 1. The binary search count is faster than the built-in count function.
+# 2. Doubling x causes a smaller increase in time taken for the binary search count function [O(log n)], compared to
+#    the built-in count function which doubles the time taken [O(n)]
+# 3. If X is changed to have more duplicate numbers, the relative efficiency of the binary search count function
+#    will still be much higher than the built-in linear count function.
+#    This is because of the binary count's O(log n) runtime complexity compared to the
+#    built-in count's O(n) linear complexity.
+#    Moreover, if we wanted to further optimize the entire sorting+searching process, we
+#    could use the "Counting Sort" algorithm to deal with the duplicate values in the
+#    sorting process.
+#    However, without the "Counting Sort" algorithm, adding duplicate/non-duplicate
+#    values to X will make minimal difference in time taken to count it provided
+#    that the number of values added to X are equal (when adding either duplicate/non-duplicate values).
+#    Additionally, even if the target number that is to be counted has a significant amount
+#    of duplicate values in X, binary search count will still uphold its performance superiority
+#    because it uses the binary search algorithm to determine the start and end of the target number streak in the sorted list.
+# 4. If larger proportions of Y is not in X, when counting the numbers from Y that are not in X,
+#    python's linear count() function's search space is still the entire list of X,
+#    even if the number is not in X.
+#    However, with binary search count, the search space/iteration count for numbers that are not in X
+#    is only log2(n+1) and not the entire size of X itself.
+# 5. The results make sense. With binary search, we sort X only once at the start of the
+#    program, and perform additional count queries on the already sorted list, drastically
+#    reducing the time complexity as the number of counts increases. With python's built-in
+#    linear count() function, it does not perform any form of pre-processing (sorting the list in advance)
+#    and has to search through every single element in X for each time it counts.
